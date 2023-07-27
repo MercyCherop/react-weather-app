7a048ffc6a0c51df1040220c9ca8b40e1a685ae9
MercyCherop <106960573+MercyCherop@users.noreply.github.com>
2023-07-27 08:24:16 +0200

    updated css

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
-import App from './App';
+import App from './Weather';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <footer>
+      This page was coded by Mercy Cherop using{" "}
+      <a href="https://github.com/MercyCherop/react-weather-app">
+        open-source code
+      </a>
+    </footer>
   </React.StrictMode>
 );
 
